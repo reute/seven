1637e4269c26a9de1e334a146920456c33a000e7
reute <j.rtl@gmx.de>
2018-12-16 16:04:46 +0100

    changes to db

--- seven/SevenPrism/Models/Sale.cs
+++ seven/SevenPrism/Models/Sale.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace SevenPrism.Models
 {
     public class Sale : BindableBase
     {
-        private DateTime _date = DateTime.Now;
+  
         public int Id { get; set; }
-     
+
+        private DateTime _date = DateTime.Now;
+        [Required]
         public DateTime Date
         {
             get => _date;
@@ -25,33 +28,33 @@ namespace SevenPrism.Models
         public Category Cat { get; set; }
 
         public string Detail { get; set; } = string.Empty;
-
+   
         public Referent Ref { get; set; }
 
-        private int? _amount;
-        public int? Amount
+        private int _amount;
+        public int Amount
         {
             get => _amount;
             set
             {
-                if (SetProperty(ref _amount, value) && _price != null)
+                if (SetProperty(ref _amount, value))
                     Sum = _amount * _price;
             }
         }
 
-        private decimal? _price;
-        public decimal? Price
+        private decimal _price;
+        public decimal Price
         {
             get => _price;
             set
             {
-                if (SetProperty(ref _price, value) && _amount != null)
+                if (SetProperty(ref _price, value))
                     Sum = _amount * _price;
             }
         }
 
-        private decimal? _sum;
-        public decimal? Sum
+        private decimal _sum;
+        public decimal Sum
         {
             get => _sum;
             
